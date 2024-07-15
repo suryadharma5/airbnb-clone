3a47aba0d3ace0aefda94b5c5098bfca0be96188
suryadharma <surya_setiawan@bca.co.id>
2024-07-15 17:07:50 +0700

    Initial Commit

---
+++ learn-swiftUI/learn_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_swiftUIApp.swift
+//  learn-swiftUI
+//
+//  Created by ITBCA on 15/07/24.
+//
+
+import SwiftUI
+
+@main
+struct learn_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
