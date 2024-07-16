047c8a78f51a7fdb9098acdf24ce9a79f2dba73e
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-07-16 17:35:54 +0700

    feat: add property detail UI

---
+++ learn-swiftUI/Core/Listings/ListingDetailView.swift
@@ -0,0 +1,216 @@
+//
+//  ListingDetailView.swift
+//  learn-swiftUI
+//
+//  Created by ITBCA on 16/07/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct ListingDetailView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .topLeading) {
+                ListingImageCarouselView()
+                    .frame(height: 320)
+                
+                Button(action: {
+                    dismiss()
+                }) {
+                    Image(systemName: "chevron.left")
+                        .foregroundStyle(.black)
+                        .background(
+                            Circle()
+                                .fill(.white)
+                                .frame(width: 32, height: 32)
+                        )
+                        // padding harus diberikan di label, agar button tetap clickable
+                        .padding(32)
+                }
+            }
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Miami Villa")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                
+                VStack(alignment: .leading) {
+                    HStack(spacing: 2) {
+                        Text("28 reviews")
+                            .underline()
+                            .fontWeight(.semibold)
+                        
+                        Text("-")
+                        
+                        Image(systemName: "star.fill")
+                        
+                        Text("4.86")
+                    }
+                    .foregroundStyle(.black)
+                    
+                    Text("Miami, Florida")
+                }
+                .font(.caption)
+            }
+            .padding(.leading)
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+            
+            Divider()
+            
+            // host info view
+            HStack {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Entire villa hosted by John Smith")
+                        .font(.headline)
+                    HStack(spacing: 2) {
+                        Text("4 guests -")
+                        Text("4 bedrooms -")
+                        Text("4 beds -")
+                        Text("3 baths")
+                    }
+                    .font(.caption)
+                }
+                .frame(width: 250, alignment: .leading)
+                
+                Spacer()
+                
+                Image("male-profile")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 64, height: 64)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            }
+            .padding()
+            
+            Divider()
+            
+            // listing feature
+            VStack {
+                ForEach(0 ..< 2) { feature in
+                    HStack(spacing: 12) {
+                        Image(systemName: "medal")
+                        
+                        VStack(alignment:.leading, content: {
+                            Text("Superhost")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                            
+                            Text("Superhost are experience, highly rated hosts who are commited to providing great stars for guests")
+                                .font(.caption)
+                                .foregroundStyle(.gray)
+                        })
+                        
+                        Spacer()
+                    }
+                }
+            }
+            .padding()
+            
+            Divider()
+            
+            // bedroom view
+            VStack(alignment: .leading) {
+                Text("Where you'll sleep")
+                    .font(.headline)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 16) {
+                        ForEach(1 ..< 5) { bedroom in
+                            VStack {
+                                Image(systemName: "bed.double")
+                                Text("Bedroom \(bedroom)")
+                            }
+                            .frame(width: 132, height: 100)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(lineWidth: 1)
+                                    .foregroundStyle(.gray)
+                                    
+                            }
+                        }
+                    }
+                }
+            }
+            .padding()
+            
+            Divider()
+            
+            // listing facility
+            VStack(alignment: .leading, spacing: 16) {
+                Text("What this place offer")
+                    .font(.headline)
+                
+                ForEach(0..<5) { facility in
+                    HStack {
+                        Image(systemName: "wifi")
+                            .frame(width: 32)
+                        
+                        Text("WiFi")
+                            .font(.footnote)
+                        
+                        Spacer()
+                    }
+                }
+            }
+            .padding()
+            
+            Divider()
+            
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Where you'll be")
+                    .font(.headline)
+                
+                Map()
+                    .frame(height: 200)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+            }
+            .padding()
+        }
+        .ignoresSafeArea(edges: [.top])
+        
+        ZStack(alignment: .bottom) {
+            VStack {
+                Divider()
+                    .padding(.bottom)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("$500")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                        
+                        Text("Total before taxes")
+                            .font(.footnote)
+                        
+                        Text("Oct 15 - 20")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                            .underline()
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Text("Reserve")
+                            .foregroundStyle(.white)
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 140, height: 40)
+                            .background(.pink)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    })
+                }
+                .padding(.horizontal, 32)
+            }
+            .background(.white)
+        }
+    }
+}
+
+#Preview {
+    ListingDetailView()
+}
--- learn-swiftUI/Core/Listings/ListingItemView.swift
+++ learn-swiftUI/Core/Listings/ListingItemView.swift
@@ -8,27 +8,12 @@
 import SwiftUI
 
 struct ListingItemView: View {
-    
-    var images = [
-        "listing-1",
-        "listing-2",
-        "listing-3",
-        "listing-4",
-    ]
-    
     var body: some View {
         VStack(spacing: 8) {
             // images
-            TabView{
-                ForEach(images, id: \.self) { image in
-                    Image(image)
-                        .resizable()
-                        .scaledToFill()
-                }
-            }
-            .frame(height: 320)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .tabViewStyle(.page)
+            ListingImageCarouselView()
+                .frame(height: 320)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
             
             // listing details
             HStack(alignment: .top) {
@@ -36,6 +21,7 @@ struct ListingItemView: View {
                 VStack(alignment: .leading) {
                     Text("Miami, Florida")
                         .fontWeight(.bold)
+                        .foregroundStyle(.black)
                     
                     Text("12 mil away")
                         .foregroundStyle(.gray)
@@ -48,6 +34,7 @@ struct ListingItemView: View {
                             .fontWeight(.bold)
                         Text("night")
                     }
+                    .foregroundStyle(.black)
                 }
                 
                 Spacer()
@@ -56,6 +43,7 @@ struct ListingItemView: View {
                     Image(systemName: "star.fill")
                     Text("4.86")
                 }
+                .foregroundStyle(.black)
                 
             }
             .font(.footnote)
